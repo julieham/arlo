bb22dffe8da89a7ae15efaba0e90e4da67647408
Julie Hamadou <dolitham@hotmail.com>
2019-02-18 11:56:27 +0100

    url redesign

--- arlo/web/ipa.py
+++ arlo/web/ipa.py
@@ -8,18 +8,22 @@ app = Flask(__name__)
 CORS(app)
 ipa = Api(app)
 
-ipa.add_resource(ListOperations, "/list")
-ipa.add_resource(CategorizeOperations, "/categorize")
-ipa.add_resource(RefreshOperations, "/refresh")
-ipa.add_resource(CreateManualTransaction, "/create")
-ipa.add_resource(LinkTwoTransactions, "/link")
+ipa.add_resource(GetRecurring, "/list/recurring")
+ipa.add_resource(GetAllCycles, "/list/cycle")
+
+ipa.add_resource(ListOperations, "/transactions")
 ipa.add_resource(GetRecap, "/recap")
 ipa.add_resource(GetBalances, "/balances")
-ipa.add_resource(MakeRecurring, "/recurring")
-ipa.add_resource(GetRecurring, "/list/recurring")
-ipa.add_resource(NameOperations, "/name")
-ipa.add_resource(changeCycle, "/cycle")
-ipa.add_resource(GetAllCycles, "/cycles")
+
+ipa.add_resource(RefreshOperations, "/refresh")
+
+ipa.add_resource(CreateManualTransaction, "/create/manual")
+ipa.add_resource(MakeRecurring, "/create/recurring")
+
+ipa.add_resource(LinkTwoTransactions, "/set-fields/link")
+ipa.add_resource(CategorizeOperations, "/set-fields/category")
+ipa.add_resource(NameOperations, "/set-fields/name")
+ipa.add_resource(changeCycle, "/set-fields/cycle")
 
 
 if __name__ == '__main__':
