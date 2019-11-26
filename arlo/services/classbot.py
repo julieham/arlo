f954d85d0aaf36cdc681465042c344ca25e84d6e
Julie Hamadou <dolitham@gmail.com>
2019-11-26 12:22:40 +0100

    classbot output reader

--- arlo/services/classbot.py
+++ arlo/services/classbot.py
@@ -1,9 +1,8 @@
-from parameters.param import directory
 from tools.logging import info
 
 
 def get_classbot_last():
-    filename = directory[:-10] + 'classbot/output.txt'
+    filename = '/home/arlo/classbot/output.txt'
     info(filename)
     with open(filename, 'r') as f:
         return f.read()
